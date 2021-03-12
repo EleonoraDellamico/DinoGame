15b229f516874c2653ad9af95529fb9aa44b9344
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-03-12 13:04:58 +0100

    added function generateObstacle

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const dino = document.querySelector('.dino');
+	const grid = document.querySelector('.grid');
 	let isJumping = false;
 	let gravity = 0.9;
 
@@ -12,30 +13,45 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 	document.addEventListener('keyup', control);
-
+	let position = 0;
 	function jump() {
 		let count = 0;
 		let timerId = setInterval(function() {
 			//move down
-			if (position === 150) {
+			if (count === 15) {
 				clearInterval(timerId);
 				console.log('down');
 				let downtimerId = setInterval(function() {
-					if (position === 0) {
+					if (count === 0) {
 						clearInterval(downtimerId);
 						isJumping = false;
 					}
-					position -= 30;
+					position -= 5;
+					count--;
+					position = position * gravity;
 					dino.style.bottom = position + 'px';
 				}, 20);
 			}
 			//move up
 			console.log('up');
-			count++;
 			position += 30;
+			count++;
 			position = position * gravity;
 			dino.style.bottom = position + 'px';
 			console.log(dino.style.bottom);
 		}, 20);
 	}
+
+	function generateObstacles() {
+		let obstaclePosition = 1000;
+		const obstacle = document.createElement('div');
+		obstacle.classList.add('obstacle');
+		grid.appendChild(obstacle);
+		obstacle.style.left = obstaclePosition + 'px';
+
+		let timerId = setInterval(function() {
+			//code
+		});
+	}
+	generateObstacles();
 });
